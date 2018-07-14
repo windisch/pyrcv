8346e78430123f478e6c5a4094a4bf2d2f886f5c
Tobias Windisch <tobias.windisch@posteo.de>
2018-07-14 14:47:12 +0200

    add more tests

--- tests/test_vote.py
+++ tests/test_vote.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+from pyrcv.vote import has_gaps
 from pyrcv.vote import Election
 
 
@@ -8,36 +9,99 @@ class TestVote(unittest.TestCase):
     def test_vote_per_candidate_ids(self):
 
         election = Election(
-            choices=["A", "B", "C", "D"])
+            candidates=["A", "B", "C", "D"])
 
         election.vote([2, 1, 3])
         election.vote([2, 0, 3])
         election.vote([0, 1, 2])
         election.vote([0, 2, 1])
-        election.vote([2, 1, 2])
+        election.vote([2, 1, 0])
 
         self.assertEquals(election.tally(), 'C')
 
-    def test_init(self):
+    def test_tally(self):
 
         election = Election(
-            choices=["candiate_a", "candidate_b", "candidate_c", "chandidate_d"])
+            candidates=["candidate_a", "candidate_b", "candidate_c", "candidate_d"])
 
         # Vote per dicts
-        election.vote({'candidate_a': 2, 'candidate_d': 1})
-        election.vote({'candidate_a': 2, 'candidate_c': 1})
+        election.vote({'candidate_a': 1, 'candidate_d': 0})
+        election.vote({'candidate_a': 1, 'candidate_c': 0})
 
         # Vote per lists
-        election.vote(['candidate_c', 'candidate_a'])
+        # election.vote(['candidate_c', 'candidate_a'])
 
         # Vote per candidate ids
         election.vote([3, 1, 4, 2])
         election.vote([1, 3, 2])  # means a, c, b
 
-        self.assertEquals(election.tally(), 'candidate_a')
+        self.assertEquals(election.tally(), 'candidate_d')
 
         # Counting again yields again the winner
-        self.assertEquals(election.tally(), 'candidate_a')
+        self.assertEquals(election.tally(), 'candidate_d')
+
+    def test_read_from_list(self):
+
+        election = Election(
+            candidates=["candidate_a", "candidate_b", "candidate_c", "candidate_d"])
+
+        np.testing.assert_array_equal(
+            [2, 0, 1, 3],
+            election._read_vote_from_list([2, 0, 1, 3]))
+
+        # No duplicate rankings
+        with self.assertRaises(ValueError):
+            election._read_vote_from_list([0, 1, 2, 1])
+
+        # Candidate IDs have to match
+        with self.assertRaises(ValueError):
+            election._read_vote_from_list([3, 1, 2, 4, 0])
+
+    def test_read_from_dict(self):
+
+        election = Election(
+            candidates=["candidate_a", "candidate_b", "candidate_c", "candidate_d"])
+
+        np.testing.assert_array_equal(
+            [3, 0],
+            election._read_vote_from_dict({'candidate_a': 1, 'candidate_d': 0}))
+
+        # No gaps
+        with self.assertRaises(ValueError):
+            election._read_vote_from_dict({'candidate_a': 0, 'candidate_c': 2})
+
+        # Only known candidates
+        with self.assertRaises(ValueError):
+            election._read_vote_from_dict({'candidate_a': 1, 'candidate_ee': 0})
+
+    def test_add_list_vote(self):
+
+        election = Election(
+            candidates=["candidate_a", "candidate_b", "candidate_c", "candidate_d"])
+
+        # Vote per candidate ids
+        election.vote([2, 0, 3, 1])
+        election.vote([0, 2, 1])  # means a, c, b
+
+        np.testing.assert_array_equal(
+            election.votes,
+            [
+                [2, 0, 3, 1],
+                [0, 2, 1]
+            ])
+
+    def test_remove_candidate_from_vote(self):
+
+        election = Election(
+            candidates=["candidate_a", "candidate_b", "candidate_c", "candidate_d"])
+
+        self.assertListEqual(
+            election._remove_candidate_from_vote([0, 1, 2, 3], 1),
+            [0, 2, 3])
+
+        self.assertListEqual(
+            election._remove_candidate_from_vote([0, 1, 2, 3], 5),
+            [0, 1, 2, 3])
 
 
 class TestCounts(unittest.TestCase):
@@ -67,6 +131,19 @@ class TestCounts(unittest.TestCase):
                 [1, 3],
             ])
 
+    def test_get_candidate_to_eliminate(self):
+        self.votes = [
+            [2, 1, 3],
+            [2, 0, 3],
+            [0, 1, 2],
+            [0, 2, 1],
+            [2, 1, 3],
+        ]
+
+        counts = self.election._count_first_ranked(self.votes)
+        lowest_candidates = self.election._get_candidates_to_eliminate(counts)
+        np.testing.assert_array_equal(lowest_candidates, ['B', 'D'])
+
     def test_count_first_ranked(self):
 
         frequencies = self.election._count_first_ranked(self.votes)
@@ -93,11 +170,21 @@ class TestCounts(unittest.TestCase):
                 'D': 3
             }))
 
-        self.assertTrue(self.election._has_winner(
+        # No absolute majority given
+        self.assertFalse(self.election._has_winner(
             {
                 'A': 11,
                 'B': 2,
                 'C': 10,
+                'D': 3
+            }))
+
+        # No absolute majority given
+        self.assertTrue(self.election._has_winner(
+            {
+                'A': 11,
+                'B': 2,
+                'C': 5,
                 'D': 3
             }))
 
@@ -107,7 +194,7 @@ class TestCounts(unittest.TestCase):
             {
                 'A': 11,
                 'B': 2,
-                'C': 10,
+                'C': 5,
                 'D': 3
             })
 
@@ -121,3 +208,19 @@ class TestCounts(unittest.TestCase):
                     'C': 10,
                     'D': 3
                 })
+
+
+class TestHelpers(unittest.TestCase):
+
+    def test_has_gaps(self):
+        self.assertTrue(has_gaps([1, 2]))
+        self.assertTrue(has_gaps([0, 1, 3]))
+        self.assertTrue(has_gaps([0, 1, 2, 10]))
+        self.assertTrue(has_gaps([-1, 0, 1, 2]))
+
+        self.assertFalse(has_gaps([0]))
+        self.assertFalse(has_gaps([0, 1]))
+        self.assertFalse(has_gaps([0, 1, 2, 3]))
+
+
+
